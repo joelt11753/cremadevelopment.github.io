9762579cb3a2780bc14d6c75a1c88bb67ab49821
Carter Barnes <carterabarnes@gmail.com>
2017-07-04 09:20:41 -0700

    Add PS script to build and serve site

---
+++ scripts/docker-up.ps1
@@ -0,0 +1,16 @@
+
+$CremaSiteId = docker ps -q --filter name=cremasite
+
+if(!$CremaSiteId) {
+    docker stop cremasite
+}
+
+docker run --rm --volume=${PWD}:/srv/jekyll -it jekyll/jekyll jekyll build
+
+docker run --name cremasite --rm -d -v ${PWD}\_site:/usr/share/nginx/html -p 8080:80 nginx
+
+# So let's automate a blog...
+
+# docker run --rm --volume=${PWD}:/srv/jekyll -it jekyll/jekyll jekyll new myblogaboutit
+
+# docker run --rm --volume=${PWD}:/srv/jekyll -it jekyll/jekyll jekyll build
